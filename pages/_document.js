bb3df5e757f0a8b9497312603a9621a29236e0a2
MSpilari <mb.spilari@hotmail.com>
2021-01-26 11:54:35 -0300

    Finalizando a 1ª aula

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,11 @@ class MyDocument extends Document {
 			<Html>
 				<Head>
 					<meta property='og:image' content={db.bg}></meta>
+					<link rel='preconnect' href='https://fonts.gstatic.com' />
+					<link
+						href='https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap'
+						rel='stylesheet'
+					/>
 				</Head>
 				<body>
 					<Main />
--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,51 @@
 import styled from 'styled-components'
 
+import BackGroundImg from '../src/components/Background'
+import Logo from '../src/components/Logo'
+import Box from '../src/components/Box'
+import GithubCorner from '../src/components/GithubCorner'
+import Footer from '../src/components/Footer'
+
 import db from '../db.json'
 
-const BackGroundImg = styled.div`
+const QuizContainer = styled.div`
 	width: 100%;
-	background-size: cover;
-	background-position: center;
-	background-image: url(${({ backgroundImage }) => backgroundImage});
-	background-color: ${({ theme }) => theme.colors.mainBg};
-	flex: 1;
+	max-width: 350px;
+	padding-top: 45px;
+	margin: auto 10%;
 	@media screen and (max-width: 500px) {
-		background-image: none;
-		&:after {
-			content: '';
-			background-size: cover;
-			background-position: center;
-			background-image: linear-gradient(
-					transparent,
-					${({ theme }) => theme.colors.mainBg}
-				),
-				url(${({ backgroundImage }) => backgroundImage});
-			display: block;
-			width: 100%;
-			height: 210px;
-			position: absolute;
-			top: 0;
-			left: 0;
-			right: 0;
-			z-index: 1;
-		}
-		*:first-child {
-			position: relative;
-			z-index: 10;
-		}
+		margin: auto;
+		padding: 15px;
 	}
 `
 
 const Home = () => {
-	return <BackGroundImg backgroundImage={db.bg}></BackGroundImg>
+	return (
+		<BackGroundImg backgroundImage={db.bg}>
+			<QuizContainer>
+				<Logo />
+				<Box>
+					<Box.Header>
+						<h1>{db.title}</h1>
+					</Box.Header>
+					<Box.Content>
+						<p>{db.description}</p>
+					</Box.Content>
+				</Box>
+
+				<Box>
+					<Box.Content>
+						<h1>Quizes da Galera</h1>
+						<p>lorem ipsum dolor sit amet...</p>
+					</Box.Content>
+				</Box>
+
+				<Footer />
+			</QuizContainer>
+
+			<GithubCorner projectUrl='https://github.com/MSpilari' />
+		</BackGroundImg>
+	)
 }
 
 export default Home
