d582659bc12aa10055b7c170651729379f19daf4
MSpilari <mb.spilari@hotmail.com>
2021-01-28 11:41:47 -0300

    Finalizando a animação da página de carregamento

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,20 +1,83 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import BackGroundImg from '../src/components/Background'
 import Logo from '../src/components/Logo'
 import QuizContainer from '../src/components/QuizContainer'
+import Box from '../src/components/Box'
+import Button from '../src/components/Button'
 
 import LoadingWidget from '../src/components/Loading'
 import ResultWidget from '../src/components/Result'
-import QuestionWidget from '../src/components/Question'
 
 import db from '../db.json'
 
 const Quiz = () => {
+	const [index, setIndex] = useState(0)
+	const [pageRendered, setPageRendered] = useState('Loading')
+	const totalOfQuestions = db.questions.length
+	let currentIndex = index
+	let currentQuestion = db.questions[currentIndex]
+	let questionId = `question__${currentIndex}`
+
+	const handleQuestion = event => {
+		event.preventDefault()
+		if (index < totalOfQuestions - 1) return setIndex(index + 1)
+		else return null
+	}
+
+	useEffect(() => {
+		setTimeout(() => {
+			return setPageRendered('Quiz')
+		}, 2000)
+	}, [])
+
 	return (
 		<BackGroundImg backgroundImage={db.bg}>
 			<QuizContainer>
 				<Logo />
-				<QuestionWidget />
+				{pageRendered === 'Loading' && <LoadingWidget />}
+				{pageRendered === 'Result' && <ResultWidget />}
+				{pageRendered === 'Quiz' && (
+					<Box>
+						<Box.Header>
+							<h1>
+								Questão {index + 1} de {totalOfQuestions}
+							</h1>
+						</Box.Header>
+						<img
+							alt='Descrição'
+							style={{
+								width: '100%',
+								height: '150px',
+								objectFit: 'cover'
+							}}
+							src={currentQuestion.image}
+						/>
+						<Box.Content>
+							<h1>{currentQuestion.title}</h1>
+							<h5>{currentQuestion.description}</h5>
+							<form onSubmit={e => handleQuestion(e)}>
+								{currentQuestion.alternatives.map((answer, answerIndex) => {
+									const alternativeId = `alternative__${answerIndex}`
+									return (
+										<Box.Topic
+											key={answerIndex}
+											as='label'
+											htmlFor={alternativeId}
+										>
+											<input
+												name={questionId}
+												type='radio'
+												id={alternativeId}
+											/>
+											{answer}
+										</Box.Topic>
+									)
+								})}
+								<Button type='submit'>Confirmar resposta</Button>
+							</form>
+						</Box.Content>
+					</Box>
+				)}
 			</QuizContainer>
 		</BackGroundImg>
 	)
--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,4 +1,24 @@
+import styled from 'styled-components'
 import Box from '../Box'
+
+const AnimatedCircle = styled.div`
+	border-top: 4px solid ${({ theme }) => theme.colors.secondary};
+	width: 50px;
+	height: 50px;
+	margin: 0 auto;
+	border-radius: 100%;
+	animation: move360 1s linear infinite;
+
+	@keyframes move360 {
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+`
+
+AnimatedCircle.Text = styled.p`
+	text-align: center;
+`
 
 const LoadingWidget = () => {
 	return (
@@ -6,7 +26,10 @@ const LoadingWidget = () => {
 			<Box.Header>
 				<h1>Carregando a diversão !!</h1>
 			</Box.Header>
-			<Box.Content>Desafio do carregamento</Box.Content>
+			<Box.Content>
+				<AnimatedCircle />
+				<AnimatedCircle.Text>Carregando...</AnimatedCircle.Text>
+			</Box.Content>
 		</Box>
 	)
 }
